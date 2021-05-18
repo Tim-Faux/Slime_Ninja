dcef0c7d31ed0dd20f9e11942cc10fdb119f24a1
DESKTOP-NG7FCV0\Tim <tim-faux@comcast.net>
2021-05-18 09:58:19 -0400

    Player now moves using WASD
    Needs a lot of refactoring and cleaning, I'll do that in the next commit

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -1,51 +1,115 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player1 : MonoBehaviour
 {
-	private const float XSpeed = 7f;
-	private const float YSpeed = 7f;
+	private const float XSpeed = 14f;
+	private const float YSpeed = 14f;
+	private const float wallCheckAdjustment = 0.25f;
 	bool hasHitObj = true;
 	Rigidbody2D rb;
 	[SerializeField] KeyCode rightKey = KeyCode.D;
 	[SerializeField] KeyCode leftKey = KeyCode.A;
 	[SerializeField] KeyCode upKey = KeyCode.W;
 	[SerializeField] KeyCode downKey = KeyCode.S;
+	Vector2 prevPos;
 
 	// Start is called before the first frame update
 	void Start()
     {
 		rb = GetComponent<Rigidbody2D>();
+		prevPos = rb.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(rightKey)) {
+	// Update is called once per frame
+	void Update()
+	{
+		//if (!isMoving()) {
+			HandleButtonPress();
+		//}
+	}
+
+	private void HandleButtonPress()
+	{
+		Vector3 pos = rb.transform.position;
+		if (Input.GetKeyDown(rightKey) && hasHitObj && !IsTouchingWallInDirection(pos, Vector2.right)) {
 			hasHitObj = false;
-			Debug.Log("Moving the player " + XSpeed + " to the right");
-			rb.velocity = new Vector2(XSpeed, rb.velocity.y);
+			MovePlayer1(XSpeed, 0);
 		}
-		else if (Input.GetKeyDown(leftKey)) {
+		else if (Input.GetKeyDown(leftKey) && hasHitObj && !IsTouchingWallInDirection(pos, Vector2.left)) {
 			hasHitObj = false;
-			Debug.Log("Moving the player " + XSpeed + " to the left");
-			rb.velocity = new Vector2(-XSpeed, rb.velocity.y);
+			MovePlayer1(-XSpeed, 0);
 		}
-		else if (Input.GetKeyDown(upKey)) {
+		else if (Input.GetKeyDown(upKey) && hasHitObj && !IsTouchingWallInDirection(pos, Vector2.up)) {
 			hasHitObj = false;
-			Debug.Log("Moving the player " + YSpeed + " to the up");
-			rb.velocity = new Vector2(rb.velocity.x, YSpeed);
+			MovePlayer1(0, YSpeed);
 		}
-		else if (Input.GetKeyDown(downKey)) {
+		else if (Input.GetKeyDown(downKey) && hasHitObj && !IsTouchingWallInDirection(pos, Vector2.down)) {
 			hasHitObj = false;
-			Debug.Log("Moving the player " + YSpeed + " to the down");
-			rb.velocity = new Vector2(rb.velocity.x, -YSpeed);
+			MovePlayer1(0, -YSpeed);
 		}
 	}
 
+	private bool IsTouchingWallInDirection(Vector3 pos, Vector2 direction)
+	{
+		int layerMask = 1 << 6;
+		layerMask = ~layerMask;
+		float raySize = 0;
+		if (direction.x != 0) {
+			Debug.Log("Checking for wall in the X direction");
+			raySize = (GetComponent<SpriteRenderer>().bounds.size.x / 2) + wallCheckAdjustment;
+			Debug.Log(raySize);
+		}
+		else if (direction.y != 0) {
+			Debug.Log("Checking for wall in the Y direction");
+			raySize = (GetComponent<SpriteRenderer>().bounds.size.y / 2) + wallCheckAdjustment;
+			Debug.Log(raySize);
+		}
+		
+		Physics2D.queriesHitTriggers = false;
+		bool results = Physics2D.Raycast(pos, direction, raySize, layerMask);
+		Debug.DrawRay(pos, direction, Color.black, 0.5f);
+		Debug.Log(results);
+		return results;
+	}
+
+	private void MovePlayer1(float HorSpeed, float VertSpeed)
+	{
+		Debug.Log("Moving the player " + HorSpeed + " right and " + VertSpeed + " up");
+		rb.velocity = new Vector2(HorSpeed, VertSpeed);
+	}
+
 	private void OnCollisionEnter2D(Collision2D collision)
-	{	
+	{
 		hasHitObj = true;
+		//Wait(0.5f);
+		//prevPos = rb.transform.position;
+		Debug.Log("The player has stoped");
+	}
+
+	private IEnumerator Wait(float time)
+	{
+		yield return new WaitForSeconds(time);
+	}
+
+	private bool isMoving()
+	{
+		Vector2 curPos = rb.transform.position;
+		Debug.Log("prevPos: " + prevPos.x + ", " + prevPos.y);
+		Debug.Log("curPos: " + curPos.x + ", " + curPos.y);
+		if (prevPos.x == curPos.x && prevPos.y == curPos.y) {
+			return false;
+		}
+		prevPos = curPos;
+		return true;
+	}
+
+	private Vector2 Round(Vector2 value, int digits)
+	{
+		float mult = Mathf.Pow(10.0f, digits);
+		float x = Mathf.Round(value.x * mult) / mult;
+		float y = Mathf.Round(value.y * mult) / mult;
+		Vector2 roundedVector = new Vector2(x, y);
+		return roundedVector;
 	}
 }
