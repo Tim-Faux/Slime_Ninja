fea2781a5f02e8a4bf3aa8299e624e59d4f5b3e2
DESKTOP-NG7FCV0\Tim <tim-faux@comcast.net>
2022-09-09 17:16:52 -0400

    Forgot to add this

--- Assets/Scripts/BeatPattern.cs
+++ Assets/Scripts/BeatPattern.cs
@@ -3,6 +3,8 @@ using System;
 [Serializable]
 public struct BeatPattern
 {
+	// Controls the pattern the beats happen to
 	public string Letter;
+	// Controls number of beats per letter
 	public int NumBeats;
 }
